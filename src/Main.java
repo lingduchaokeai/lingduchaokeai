cc07b436997e5687e21919eaa440e47e6fdde72d
lingduchaokeai <2498395445@qq.com>
2019-02-18 10:10:29 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // 版本控制系统:VCS, version Control System
+        // 1.集中式版本控制系统: SVN,Subversion
+        // 2.分布式版本控制系统: Git
+        // 区别,集中式只能向服务器提交,自身不能当做服务器,但是分布式可以
+
+        // SVN服务器: taoCode,SVNChina
+        // Git服务器: gitee,github
+
+        // Git使用
+
+        // Git安装
+        // 下载地址: https://git-scm.com
+
+        // 初始化版本库
+        // git init
+        // 注: 当前文件夹中,有文件要进行版本控制
+
+        // 查看当前版本库的状态
+        // git status
+
+        // 指定某个文件进行版本控制
+        //
+
+
+    }
+}
